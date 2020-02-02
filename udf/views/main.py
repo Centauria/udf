997843847b1b9df6ef657a1f4836159c076ed4a0
Centauria <centauriavc@gmail.com>
2020-02-02 18:04:33 +0800

    add render

--- udf/views/main.py
+++ udf/views/main.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
-def hello(request):
-    return HttpResponse('Hello world!')
+def index(request):
+    return render(request, 'index.html')
